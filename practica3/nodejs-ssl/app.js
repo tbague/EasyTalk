7c2e141f5df1e987ec4b6d4f2334aa1fc3679e7d
proura <pau@brainupdaters.net>
2017-01-16 21:06:19 +0100

    servidor node js amb connexio segura

--- practica3/nodejs-ssl/app.js
+++ practica3/nodejs-ssl/app.js
@@ -1,43 +1,13 @@
 
 // importar
 var express = require("express");
-
-// instanciar
-var app = express();
-var https = require("https");
 var fs = require("fs");
-var io = require("socket.io")(https);
+var options = { key: fs.readFileSync('./certs/WebRTC.key'), cert: fs.readFileSync('./certs/WebRTC.crt')};
+var app = express();
+var https = require("https").Server(options, app);
 
-// ruteo
 app.use('/', express.static(__dirname + '/public_html'));
 
-/*
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-    //console.log('message: ' + msg);
-  });
-});
-*/
-
-var options = {key: fs.readFileSync("192.168.100.13.key"), cert: fs.readFileSync("192.168.100.13.crt")}
-var server = https.createServer(options, app);
-
-server.listen(3443, function(){
+https.listen(3443, function(){
   console.log("Listening on *:3443");
 });
-
-
-/*
-var express = require("express");
-var https = require("https");
-var fs = require("fs");
-var app = express();
-app.get("/", function(req, res){
-res.send("<h1>hello<h2>");
-});
-var options = {key: fs.readFileSync("192.168.100.13.key"), cert: fs.readFileSync("192.168.100.13.crt")}
-var server = https.createServer(options, app);
-console.log("Listen to 3443");
-server.listen(3443);
-*/
